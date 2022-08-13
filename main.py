5b13bbd58c81be613867b1ec8a467558b6997049
Kim DoKyun <51306225+jasper200207@users.noreply.github.com>
2022-08-14 07:48:58 +0900

    make api

---
+++ main.py
@@ -0,0 +1,25 @@
+import datetime
+import os
+import secrets
+
+from fastapi import FastAPI, UploadFile, File
+from human_detection import detection as HumanDetection
+
+app = FastAPI()
+
+@app.post("/human-detection")
+async def create_upload_files(file: UploadFile = File(...)):
+    UPLOAD_DIRECTORY = "./image"
+    contents = await file.read()
+    currentTime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    saved_file_name = ''.join([currentTime, secrets.token_hex(16), file.filename])
+    with open(os.path.join(UPLOAD_DIRECTORY, saved_file_name), "wb") as fp:
+        fp.write(contents)
+    result = []#await HumanDetection.detectImage(os.path.join(UPLOAD_DIRECTORY,saved_file_name))
+    os.remove(os.path.join(UPLOAD_DIRECTORY,saved_file_name))
+    print(result)
+    return result
+
+@app.get("/")
+async def helloWorld():
+    return "hi, I'm AI API"
